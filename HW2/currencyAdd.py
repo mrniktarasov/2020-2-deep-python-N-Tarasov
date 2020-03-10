5d652dc8752111d41c9f1fda268b28f12b315624
Nikita Tarasov <nikitatarasov987@gmail.com>
2020-03-10 18:38:58 +0300

    Доделан сумматор валюты

--- HW2/currencyAdd.py
+++ HW2/currencyAdd.py
@@ -6,16 +6,12 @@ class CurrencyAdder:
     def __init__(self, curr, name='RUB'):
         self.value = curr
         self.name = name
-        if name != 'RUB':
-            self.rub = self._to_local_current()
-        else:
-            self.rub = curr
 
     def __repr__(self):
-        return '{value: ' + str(self.value) + ', name: ' + str(self.name) + ', rub: ' + str(self.rub) + '}'
+        return '{value: ' + str(self.value) + ', name: ' + str(self.name) + '}'
 
     def __str__(self):
-        return 'Currency (value= ' + str(self.value) + ', age= ' + self.name + ', rub= ' + str(self.rub) + ')'
+        return 'Currency (value= ' + str(self.value) + ', name= ' + self.name + ')'
 
     def __add__(self, other):
         try:
@@ -25,12 +21,16 @@ class CurrencyAdder:
                 rates = self._get_currency()
                 if self.name != 'RUB':
                     try:
-                        curr1 = rates[other.name].value
+                        curr1 = rates[self.name].value
                     except KeyError as e:
                         raise ValueError('Undefined unit: {}'.format(e.args[0]))
                 else:
                     curr1 = 1
-                return round(float((self.rub + other.rub)/curr1), 2)
+                try:
+                    curr2 = rates[other.name].value
+                except KeyError as e:
+                    raise ValueError('Undefined unit: {}'.format(e.args[0]))
+                return round(float((self.value * curr1 + other.value * curr2)/curr1), 2)
         except AttributeError:
             return self.value + other
 
@@ -44,11 +44,3 @@ class CurrencyAdder:
             'CHF': rates['CHF'],
             'CNY': rates['CNY'],
         }
-
-    def _to_local_current(self):
-        rates = self._get_currency()
-        try:
-            cur = rates[self.name].value
-        except KeyError as e:
-            raise ValueError('Undefined unit: {}'.format(e.args[0]))
-        return round(float(self.value * cur), 2)
--- HW2/currencyAdd.test.py
+++ HW2/currencyAdd.test.py
@@ -8,14 +8,17 @@ class MyTestCase(unittest.TestCase):
         c2 = CurrencyAdder(5, 'USD')
         self.assertEqual(c1 + c2, 341.59)
         self.assertEqual(c1 + 6, 10)
+        c1 = CurrencyAdder(4, 'USD')
+        c2 = CurrencyAdder(5, 'CNY')
+        self.assertEqual(c1 + c2, 11.2)
 
     def test_repr(self):
         c1 = CurrencyAdder(5, 'USD')
-        self.assertEqual(c1.__repr__(), '{value: 5, name: USD, rub: %s}' % str(c1.rub))
+        self.assertEqual(c1.__repr__(), '{value: 5, name: USD}')
 
     def test_str(self):
         c1 = CurrencyAdder(5, 'USD')
-        self.assertEqual(c1.__str__(), 'Currency (value= 5, age= USD, rub= %s)' % str(c1.rub))
+        self.assertEqual(c1.__str__(), 'Currency (value= 5, name= USD)')
 
 
 if __name__ == '__main__':
